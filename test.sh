e41823d41a9e20b2f663f4859e4c65c1a9076013
Nakasone Fumiya <n18019@std.it-college.ac.jp>
2018-05-11 07:23:48 +0000

    ２０日より後の日付の処理を追加

--- test.sh
+++ test.sh
@@ -5,9 +5,11 @@ today=$(date '+%d')
 #echo $today
 
 if [$today -eq 20]; then
-  echo "今日は２０日です。月次〆処理を開始してください。"
+  echo "今日は２０日です。月次〆処理を開始してください"
 elif [$today -lt 20]; then
-  echo "今日は２０日より前の日にちです。"
+  echo "今日は２０日より前の日にちです"
+elif [$today -gt 20]; then
+  echo"今日は２０日よりあとの日にちです"
 else
   echo "今日は２０日ではありません。"
 fi
